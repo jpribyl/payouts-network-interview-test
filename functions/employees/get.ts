42c0b4ad1e10f08f0f02afb4bd3d7e982d1638c1
jpribyl <pribylsnbits@gmail.com>
2019-08-31 17:48:04 -0600

    format the way gateway wants

--- functions/employees/get.ts
+++ functions/employees/get.ts
@@ -1,16 +1,18 @@
-import { APIGatewayEvent } from "aws-lambda"
-import knex from "../../configs/dbConfig.ts"
+import { APIGatewayEvent } from 'aws-lambda';
+import knex from '../../configs/dbConfig.ts';
 
 export const getEmployees = async function(
   event: APIGatewayEvent
 ): Promise<any> {
-  console.log(event)
+  console.log(event);
 
-  const { page } = { ...event.queryStringParameters }
-  const r = await knex('employees').paginate(page)
+  const { page } = { ...event.queryStringParameters };
+  const r = await knex('employees').paginate(page);
 
   return {
-    status: 200,
-    body: JSON.stringify(r)
-  }
-}
+    statusCode: 200,
+    body: JSON.stringify(r),
+    headers: {},
+    isBase64Encoded: false,
+  };
+};
